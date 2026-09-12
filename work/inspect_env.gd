3dc50d11a573246c7e1f9537513d9e3bc9d1d199
Minjae Jeong <mjayj9@gmail.com>
2026-09-12 22:26:42 +0900

    work: build, export and validation logs

---
+++ work/inspect_env.gd
@@ -0,0 +1,5 @@
+extends SceneTree
+func _initialize():
+ for name in ClassDB.class_get_integer_constant_list("Environment"):
+  if "REFLECT" in name:print(name," = ",ClassDB.class_get_integer_constant("Environment",name))
+ quit()
---
+++ work/inspect_shadows.gd
@@ -0,0 +1,5 @@
+extends SceneTree
+func _initialize():
+ for m in ClassDB.class_get_method_list("RenderingServer"):
+  if m.name in ["directional_shadow_atlas_set_size","directional_soft_shadow_filter_set_quality"]:print(m)
+ quit()
